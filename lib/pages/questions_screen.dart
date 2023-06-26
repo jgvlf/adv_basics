35b1102b54a9b6b63b000a1517d878e58780eeb4
joaogabrielvlf <joaogabrielvlf@gmail.com>
2023-06-26 17:17:24 -0300

    DEV: Make Answer Button

--- lib/pages/questions_screen.dart
+++ lib/pages/questions_screen.dart
@@ -1,3 +1,6 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/utils/answer_button.dart';
+import 'package:adv_basics/utils/styled_text.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -12,6 +15,35 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text("Question Text");
+    final currentQuestions = questions[0];
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        StyledText(
+          currentQuestions.text,
+          color: Colors.white,
+          fontSize: 16,
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        AnswerButton(
+          currentQuestions.answers[0],
+          () {},
+        ),
+        AnswerButton(
+          currentQuestions.answers[1],
+          () {},
+        ),
+        AnswerButton(
+          currentQuestions.answers[2],
+          () {},
+        ),
+        AnswerButton(
+          currentQuestions.answers[3],
+          () {},
+        ),
+      ],
+    );
   }
 }
---
+++ lib/utils/answer_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(
+    this.title,
+    this.onPressed, {
+    super.key,
+  });
+
+  final String title;
+  final void Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 40,
+        ),
+        backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+      ),
+      child: Text(
+        title,
+      ),
+    );
+  }
+}
