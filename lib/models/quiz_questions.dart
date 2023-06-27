cde2ab96c967a4cf1cc92730cc1754209de51f51
joaogabrielvlf <joaogabrielvlf@gmail.com>
2023-06-27 17:50:45 -0300

    DEV: Add Shuffle List Function

--- lib/models/quiz_questions.dart
+++ lib/models/quiz_questions.dart
@@ -3,4 +3,10 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  List<String> getShuffleAnswers() {
+    final List<String> shuffleAnswers = List.of(answers);
+    shuffleAnswers.shuffle();
+    return shuffleAnswers;
+  }
 }
